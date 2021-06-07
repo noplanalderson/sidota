877fd8bdea7eb2d7417c2b6d34c7e4e62296c284
noplanalderson <noplanalderson@gmail.com>
2021-06-07 19:41:24 +0700

    Set "sess_match_ip" & "sess_regenerate_destroy" to FALSE

--- app/config/config.php
+++ app/config/config.php
@@ -411,9 +411,9 @@ $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'sidota_session';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = sys_get_temp_dir();
-$config['sess_match_ip'] = TRUE;
+$config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = TRUE;
+$config['sess_regenerate_destroy'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
