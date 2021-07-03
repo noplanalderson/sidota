cf0954ebcd5a85461ed2f10367bc77d40da0eb9a
noplanalderson <noplanalderson@gmail.com>
2021-07-03 22:10:25 +0700

    Update Footer Style

--- app/views/_partial/footer.php
+++ app/views/_partial/footer.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');?>
 		<div class="az-footer mb-2">
-	      <div class="container">
-	        <span><?= $this->app->footer_text;?> &copy; <?= date('Y');?> | Version : <?= APP_VERSION ?>
+	      <div class="container text-dark footer-bg p-md-2 rounded">
+	        <span><small><?= $this->app->footer_text;?> &copy; <?= date('Y');?> | Version : <?= APP_VERSION ?></small>
 	        </span>
-	        <span>Developed by: @_debu_semesta</span>
+	        <span><small>Developed by: @_debu_semesta</small></span>
 	      </div><!-- container -->
 	    </div><!-- az-footer -->
