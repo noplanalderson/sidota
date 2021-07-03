bdabfc61c2b0090512ab0f29262cb81d3e8cb842
noplanalderson <noplanalderson@gmail.com>
2021-07-03 22:15:20 +0700

    Add Background Managemet JS

---
+++ _/js/page_js/background.js
@@ -0,0 +1,75 @@
+    var baseURI = $('base').attr('href');
+
+    $('#background_tbl').DataTable( {
+        'info': false,
+        'searchable':true,
+        'responsive': true,
+        'lengthMenu': [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
+        "language": {
+          "zeroRecords": "No Background Found"
+        },
+        "dom": '<"left"l>rtip',
+    });
+
+    $(function(){
+        $('.add-background').on('click', function() {
+        	$('.modal-title').html('Add Background');
+            $('.modal-footer button[type=submit]').html('Add');
+            var url = baseURI + $('.add-background').attr('id');
+        
+            $(".picture_upload").fileinput({
+                theme: "fas",
+                uploadUrl: url,
+                showRemove: false,
+                showUpload: false,
+                fileActionSettings: {
+                    showZoom: false,
+                    showDrag: false
+                },
+                showBrowse: false,
+                browseOnZoneClick: true,
+                showCancel: true,
+                autoReplace: false,
+                maxFileCount: 10,
+                append: true,
+                autoOrientImage: true
+            });  
+        });   
+    });
+
+    $("#background_tbl").on('click', '.delete-btn', function(){
+        var result = confirm("Are You Sure to Delete Image?");
+
+        if (result) {
+            var $tr = $(this).closest('tr');
+            const image_hash = $(this).data('id');
+            var url = $('.delete-btn').attr('id');
+
+            $.ajax({
+                url: baseURI + url,
+                data: {
+                        hash: image_hash, 
+                        debu_token: $('.csrf_token').attr('value')
+                    },
+                method: 'post',
+                dataType: 'json',
+                success: function(data) {
+                    $('.csrf_token').val(data.token);
+                    $('meta[name="X-CSRF-TOKEN"]').attr('content', data.token);
+
+                    $('.delete_msg').html(data.msg);
+                    $("#delete_msg").slideDown('slow');
+                    
+                    if (data.result == 1) {
+                        $('#delete_msg').attr('class', 'alert alert-success');
+                        $tr.find('td').fadeOut(1000,function(){ 
+                            $tr.remove();                    
+                        });
+                    } else {
+                        $('#delete_msg').attr('class', 'alert alert-danger');
+                    }
+                    $("#delete_msg").alert().delay(6000).slideUp('slow');
+                }
+            });
+        }
+    });
